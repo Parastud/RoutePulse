4223e73e0f72ce58318508516c6b9e4dff679e7f
Parth Sharma <68681752+Parastud@users.noreply.github.com>
2026-04-05 14:25:33 +0530

    Update db.js

--- Backend/config/db.js
+++ Backend/config/db.js
@@ -3,7 +3,7 @@ mongoose.set('debug', true);
 
 const connectDB = async () => {
     try {
-        await mongoose.connect(`mongodb+srv://dtc:Aman2003@cluster0.76mqa.mongodb.net/DTC`);
+        await mongoose.connect(`process.env.MONGO_URI`);
         console.log('MongoDB connected successfully');
     } catch (error) {
         console.error('MongoDB connection failed:', error.message);
